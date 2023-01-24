dfad8a4fd916ddf27a9ddd51f5086ffdd1eda1a3
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-01-24 09:08:10 -0500

    Some annotation and documentation improvements

--- src/tahoe_capabilities/parser.py
+++ src/tahoe_capabilities/parser.py
@@ -1,3 +1,11 @@
+
+from __future__ import annotations
+
+__all__ = [
+    "capability_from_string",
+    "ParseError",
+]
+
 from base64 import b32decode as _b32decode
 from typing import Callable, Dict, List, TypeVar, cast, Optional
 
@@ -106,7 +114,19 @@ def writeable_directory_from_string(s: str) -> DirectoryWriteCapability:
     return cap
 
 
-def _b32string(alphabet: str, exact_bits: Optional[int] = None) -> Parser:
+def _b32string(alphabet: str, exact_bits: Optional[int] = None) -> Parser[list[str]]:
+    """
+    Parse a base32-encoded string.
+
+    :param alphabet: The alphabet to use.  Must be 32 characters long.
+
+    :exact_bits: See ``b32string``.
+
+    :return: A parser that consumes and returns the matched base32 characters
+        (still encoded).
+    """
+    assert len(alphabet) == 32
+
     if exact_bits is None:
         return many(one_of(alphabet))
 
@@ -123,7 +143,18 @@ def _b32string(alphabet: str, exact_bits: Optional[int] = None) -> Parser:
     )
 
 
-def b32string(exact_bits: Optional[int] = None) -> Parser:
+def b32string(exact_bits: Optional[int] = None) -> Parser[bytes]:
+    """
+    Parse a base32-encoded string.
+
+    :param alphabet: The alphabet to use.  Must be 32 characters long.
+
+    :param exact_bits: If ``None`` parse a string of any length.  Otherwise,
+        parse a base32 string that represents an encoded string of exactly
+        this many bits.
+
+    :return: A parser that consumes and results in the decoded string.
+    """
     # RFC3548 standard used by Gnutella, Content-Addressable Web, THEX, Bitzi,
     # Web-Calculus...
     rfc3548_alphabet = "abcdefghijklmnopqrstuvwxyz234567"
@@ -134,31 +165,56 @@ def b32string(exact_bits: Optional[int] = None) -> Parser:
 
 
 def _trailing_b32chars(alphabet: str, bits: int) -> str:
+    """
+    Find the part of the base32 alphabet that is required and allowed to
+    express the given number of bits of encoded data.
+
+    This is used to match the end of a base32 string where the length of the
+    encoded data is not a multiple of 5 bits (the base32 word size).
+    """
     stem = alphabet[:: 1 << bits]
     if bits == 0:
         return stem
     return stem + _trailing_b32chars(alphabet, bits - 1)
 
 
+# Match the common separator between components of capability strings.
 _sep = string(":")
+
+# Match a natural number
 _natural = many1(digit()).parsecmap("".join).parsecmap(int)
+
+# Match a base32-encoded binary string with the length of a key.
 _key = b32string(128)
+
+# Match a base32-encoded binary string with the length of a fingerprint or UEB hash.
 _uri_extension_hash = b32string(256)
 
+# Match the base32-encoded data portion of a literal capability.  This is not
+# length limited though in practice literals are only used for data of 55
+# bytes or less.
 _lit = b32string()
 
+# Match the needed shares, total shares, and data size numbers in a CHK
+# capability string.
 _chk_params = times(_sep >> _natural, 3, 3)
+
+# Match all of the data components (but not the type prefix) of a CHK
+# capability string.
 _chk = _key + (_sep >> _uri_extension_hash) + _chk_params
 
+# Match all of the data components (but not the type prefix) of an SSK (SDMF
+# or MDMF) capability string.
+#
 # Tahoe-LAFS calls the components of SSK "storage_index" and "fingerprint" but
 # they are syntactically the same as "key" and "uri_extension_hash" from CHK.
 _ssk = _key + (_sep >> _uri_extension_hash)
 
-# And MDMF is syntactically compatible with SSK
-_mdmf = _ssk
 
-
-def _capability_parser() -> Parser:
+def _capability_parser() -> Parser[Capability]:
+    """
+    Parse any kind of capability string.
+    """
     lit_read = string("LIT:") >> _lit.parsecmap(LiteralRead)
 
     def chk_glue(f):
@@ -175,9 +231,9 @@ def _capability_parser() -> Parser:
     ssk_read = string("SSK-RO:") >> _ssk.parsecmap(lambda p: SSKRead.derive(*p))
     ssk = string("SSK:") >> _ssk.parsecmap(lambda p: SSKWrite.derive(*p))
 
-    mdmf_verify = string("MDMF-Verifier:") >> _mdmf.parsecmap(lambda p: MDMFVerify(*p))
-    mdmf_read = string("MDMF-RO:") >> _mdmf.parsecmap(lambda p: MDMFRead.derive(*p))
-    mdmf = string("MDMF:") >> _mdmf.parsecmap(lambda p: MDMFWrite.derive(*p))
+    mdmf_verify = string("MDMF-Verifier:") >> _ssk.parsecmap(lambda p: MDMFVerify(*p))
+    mdmf_read = string("MDMF-RO:") >> _ssk.parsecmap(lambda p: MDMFRead.derive(*p))
+    mdmf = string("MDMF:") >> _ssk.parsecmap(lambda p: MDMFWrite.derive(*p))
 
     def dir2(file_parser: Parser, dir_type: type) -> Parser:
         return string("DIR2-") >> file_parser.parsecmap(dir_type)
@@ -220,4 +276,7 @@ _capability = _capability_parser()
 
 
 def capability_from_string(s: str) -> Capability:
+    """
+    Parse any known capability string.
+    """
     return _capability.parse(s)
